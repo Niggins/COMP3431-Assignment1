50d847ee508b6d734016fb90c86444732e137679
Alex Pentland <apentlander@gmail.com>
2013-09-11 23:52:53 +1000

    idk

--- comp3431/src/assign1_2013/src/nodes/detector.cpp
+++ comp3431/src/assign1_2013/src/nodes/detector.cpp
@@ -5,6 +5,7 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <cv.h>
+#include <vector>
 namespace enc = sensor_msgs::image_encodings;
 
 static const char WINDOW[] = "Image window";
@@ -15,8 +16,11 @@ class ImageConverter
   image_transport::ImageTransport it_;
   image_transport::Subscriber image_sub_;
   image_transport::Publisher image_pub_;
-  cv::Mat src, proc, out, erosion, dilation, elementRect, elementCross, coloured;
+  cv::Mat src, proc, out;
 
+  cv::Mat threshold_output, drawing;
+  std::vector<std::vector<cv::Point> > contours;
+  std::vector<cv::Vec4i> hierarchy;
 
 public:
   ImageConverter()
@@ -24,15 +28,13 @@ public:
   {
     image_pub_ = it_.advertise("out", 1);
     image_sub_ = it_.subscribe("in", 1, &ImageConverter::imageCb, this);
+
     cv::namedWindow(WINDOW);
-    cv::namedWindow("Erosion");
-    cv::namedWindow("No noise");
-    cv::namedWindow("Coloured");
   }
 
   ~ImageConverter()
   {
-    cv::destroyAllWindows();
+    cv::destroyWindow(WINDOW);
   }
 
   void imageCb(const sensor_msgs::ImageConstPtr& msg)
@@ -49,41 +51,47 @@ public:
       ROS_ERROR("cv_bridge exception: %s", e.what());
       return;
     }
-    image_pub_.publish(cv_ptr->toImageMsg()); //needs to be moved
-   image_processing();
-  }
 
-  void image_processing(){
     cv::cvtColor(src, proc, CV_BGR2HSV);
-    cv::blur(proc, proc, cv::Size(3, 3));
-    cv::inRange(proc, cv::Scalar(158, 120, 120), cv::Scalar(167, 200, 200), out);
- //   cv::blur(out, out, cv::Size(3, 3));
-    //erozja
+    cv::blur(proc, proc, cv::Size(4, 4));
 
-       //setting dimensions of new images
-    erosion.create(proc.rows, proc.cols, proc.type());
-    dilation.create(proc.rows, proc.cols, proc.type());
+    cv::Mat pinkOut, blueOut, greenOut, yellowOut, pbOut, gyOut;
+    cv::inRange(proc, cv::Scalar(158, 120, 120), cv::Scalar(168, 200, 200), pinkOut);
+    cv::inRange(proc, cv::Scalar(100, 120, 120), cv::Scalar(123, 170, 200), blueOut);
+    cv::inRange(proc, cv::Scalar(44, 120, 120), cv::Scalar(65, 170, 200), greenOut);
+    cv::inRange(proc, cv::Scalar(30, 120, 120), cv::Scalar(40, 170, 200), yellowOut);
+    cv::bitwise_or(pinkOut, blueOut, pbOut);
+    cv::bitwise_or(greenOut, yellowOut, gyOut);
+    cv::bitwise_or(pbOut, gyOut, threshold_output);
 
-       int numberOfIterations = 5;
-       int erosion_size=1; //width of the "brush" for the process
-       elementCross = getStructuringElement( cv::MORPH_CROSS, cv::Size( 2*erosion_size + 1, 2*erosion_size+1 ),
-                                              cv::Point( erosion_size, erosion_size ) );
-       elementRect = getStructuringElement( cv::MORPH_RECT, cv::Size( 2*erosion_size + 1, 2*erosion_size+1 ),
-                                                     cv::Point( erosion_size, erosion_size ) );
 
-       //erozja wlasciwa
-       erode(out, erosion, elementCross);
-       medianBlur(erosion, erosion, 5);
-       dilate(erosion, dilation, elementRect);
 
-//       cv::imshow("Erosion", erosion);
-       cv::imshow("No noise", dilation);
+    cv::findContours(threshold_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0));
+    std::vector<cv::RotatedRect> minRect(contours.size());
+    for(int i = 0; i < contours.size(); i++)
+    { 
+      minRect[i] = cv::minAreaRect(cv::Mat(contours[i]));
+    }
 
- //   cv::imshow(WINDOW, out);
+    cv::Scalar contColor = cv::Scalar(255, 0, 0);
+    cv::Scalar recColor = cv::Scalar(0, 0, 255);
+    cv::Scalar becColor = cv::Scalar(0, 255, 0);
+    drawing = cv::Mat::zeros( threshold_output.size(), CV_8UC3 );
+    for(int i = 0; i< contours.size(); i++ )
+    {
+      // contour
+      cv::drawContours(drawing, contours, i, contColor, 1, 8, std::vector<cv::Vec4i>(), 0, cv::Point());
+      // rotated rectangle
+      cv::Point2f rect_points[4]; minRect[i].points(rect_points);
+      for(int j = 0; j < 4; j++ )
+        cv::line(drawing, rect_points[j], rect_points[(j+1)%4], recColor, 1, 8 );
+    }
+
+    cv::imshow(WINDOW, drawing);
     cv::waitKey(3);
 
+    image_pub_.publish(cv_ptr->toImageMsg());
   }
-
 };
 
 int main(int argc, char** argv)
