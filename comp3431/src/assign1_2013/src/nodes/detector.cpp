3c0d323d9e305c7e018208797cddf3d06f55c311
Alex Pentland <apentlander@gmail.com>
2013-09-12 05:01:09 +1000

    It now detects when colors are together and which is on top

--- comp3431/src/assign1_2013/src/nodes/detector.cpp
+++ comp3431/src/assign1_2013/src/nodes/detector.cpp
@@ -5,8 +5,8 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <cv.h>
+#include <string>
 #include <vector>
-#include <sstream>
 namespace enc = sensor_msgs::image_encodings;
 
 static const char WINDOW[] = "Image window";
@@ -56,57 +56,86 @@ public:
     cv::cvtColor(src, proc, CV_BGR2HSV);
     cv::GaussianBlur(proc, proc, cv::Size(3, 3), 0, 0);
 
-    cv::Mat pinkOut, blueOut, greenOut, yellowOut, pbOut, gyOut;
-    cv::inRange(proc, cv::Scalar(158, 120, 120), cv::Scalar(168, 200, 200), pinkOut);
-    cv::inRange(proc, cv::Scalar(100, 127, 77), cv::Scalar(110, 192, 179), blueOut);
-    cv::inRange(proc, cv::Scalar(75, 127, 35), cv::Scalar(90, 230, 110), greenOut);
-    cv::inRange(proc, cv::Scalar(22, 178, 102), cv::Scalar(30, 255, 179), yellowOut);
-    cv::bitwise_or(pinkOut, blueOut, pbOut);
-    cv::bitwise_or(greenOut, yellowOut, gyOut);
-    cv::bitwise_or(pbOut, gyOut, threshold_output);
+    cv::Mat colorOut[4], combOut[2];
+    cv::Scalar boxColor[4];
+    // Pink Layer
+    cv::inRange(proc, cv::Scalar(158, 120, 120), cv::Scalar(168, 200, 200), colorOut[0]);
+    boxColor[0] = cv::Scalar(255, 20, 147);
+    // Blue Layer
+    cv::inRange(proc, cv::Scalar(100, 127, 77), cv::Scalar(110, 192, 179), colorOut[1]);
+    boxColor[1] = cv::Scalar(0, 0, 255);
+    // Green layer
+    cv::inRange(proc, cv::Scalar(75, 127, 35), cv::Scalar(90, 230, 110), colorOut[2]);
+    boxColor[2] = cv::Scalar(0, 255, 0);
+    // Yellow layer
+    cv::inRange(proc, cv::Scalar(22, 178, 102), cv::Scalar(30, 255, 179), colorOut[3]);
+    boxColor[3] = cv::Scalar(255, 255, 0);
+    cv::String name[4] = {"Pink", "Blue", "Green", "Yellow"};
+    /*cv::bitwise_or(colorOut[0], colorOut[1], combOut[0]);
+    cv::bitwise_or(colorOut[2], colorOut[3], combOut[1]);
+    cv::bitwise_or(combOut[0], combOut[1], threshold_output);*/
 
     //setting dimensions of new images
     erosion.create(proc.rows, proc.cols, proc.type());
     dilation.create(proc.rows, proc.cols, proc.type());
 
-    int numberOfIterations = 5;
-    int erosion_size=1; //width of the "brush" for the process
-    elementCross = cv::getStructuringElement( cv::MORPH_CROSS, cv::Size( 2*erosion_size + 1, 2*erosion_size+1 ),
-                                        cv::Point( erosion_size, erosion_size ) );
-    elementRect = cv::getStructuringElement( cv::MORPH_RECT, cv::Size( 2*erosion_size + 1, 2*erosion_size+1 ),
-                                               cv::Point( erosion_size, erosion_size ) );
-
-    //erozja wlasciwa
-    cv::erode(threshold_output, erosion, elementCross);
-    cv::medianBlur(erosion, erosion, 5);
-    cv::dilate(erosion, threshold_output, elementRect);
-
-    //cv::CvFont font = cv::fontQt("Arial");
-
-    cv::findContours(threshold_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0));
-    //std::vector<cv::RotatedRect> minRect(contours.size());
-    std::vector<cv::Rect> minRect(contours.size());
-    for(int i = 0; i < contours.size(); i++)
-    { 
-      minRect[i] = cv::boundingRect(cv::Mat(contours[i]));
-      minRect[i].height += 6;
-    }
-
-    cv::Scalar contColor = cv::Scalar(255, 0, 0);
-    cv::Scalar recColor = cv::Scalar(0, 0, 255);
-    cv::Scalar becColor = cv::Scalar(0, 255, 0);
-    drawing = cv::Mat::zeros( threshold_output.size(), CV_8UC3 );
-    for(int i = 0; i< contours.size(); i++ )
+    std::vector<cv::Rect> pinkRect;
+    for (int k=0; k < 4; k++)
     {
-      // contour
-      cv::drawContours(drawing, contours, i, contColor, 1, 8, std::vector<cv::Vec4i>(), 0, cv::Point());
-      cv::rectangle(drawing, minRect[i], recColor);
-      //cv::Point midRight(minRect[i].x + minRect[i].width, minRect[i].y);
-      //std::string width;
-      //cv::putText(drawing, std::to_string(minRect[i].width), midRight, cv::FONT_HERSHEY_SCRIPT_SIMPLEX, 2, becColor);
+      int numberOfIterations = 5;
+      int erosion_size=1; //width of the "brush" for the process
+      elementCross = cv::getStructuringElement(cv::MORPH_CROSS, cv::Size( 2*erosion_size + 1, 2*erosion_size + 1),
+                                               cv::Point(erosion_size, erosion_size));
+      elementRect = cv::getStructuringElement(cv::MORPH_RECT, cv::Size( 2*erosion_size + 1, 2*erosion_size + 1 ),
+                                              cv::Point(erosion_size, erosion_size));
+
+      //erozja wlasciwa
+      cv::erode(colorOut[k], erosion, elementCross);
+      cv::medianBlur(erosion, erosion, 5);
+      cv::dilate(erosion, colorOut[k], elementRect);
+
+      cv::findContours(colorOut[k], contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0));
+      std::vector<cv::Rect> minRect(contours.size());
+      for(int i = 0; i < contours.size(); i++)
+      { 
+        minRect[i] = cv::boundingRect(cv::Mat(contours[i]));
+        minRect[i].height += 7;
+
+        if (k == 0)
+        {
+          pinkRect.push_back(minRect[i]);
+        }
+        else
+        {
+          for (int a = 0; a < pinkRect.size(); a++)
+          {
+            if ((pinkRect[a] & minRect[i]).area() && (k == 1))
+            {
+              cv::Point circ;
+              if (pinkRect[a].y > minRect[i].y)
+                circ = cv::Point(pinkRect[a].x, pinkRect[a].y);
+              else
+                circ = cv::Point(minRect[i].x, minRect[i].y);
+              cv::circle(colorOut[k], circ, 3, cv::Scalar(255, 0, 0), -1);
+            }
+          }
+        }
+      }
+
+      drawing = cv::Mat::zeros(colorOut[k].size(), CV_8UC3);
+      for(int i = 0; i < contours.size(); i++ )
+      {
+        // contour
+        //cv::drawContours(drawing, contours, i, contColor, 1, 8, std::vector<cv::Vec4i>(), 0, cv::Point());
+        cv::rectangle(colorOut[k], minRect[i], boxColor[k]);
+      }
     }
 
-    cv::imshow(WINDOW, drawing);
+    cv::bitwise_or(colorOut[0], colorOut[1], combOut[0]);
+    cv::bitwise_or(colorOut[2], colorOut[3], combOut[1]);
+    cv::bitwise_or(combOut[0], combOut[1], threshold_output);
+
+    cv::imshow(WINDOW, threshold_output);
     cv::waitKey(3);
 
     image_pub_.publish(cv_ptr->toImageMsg());
