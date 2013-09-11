8ff094a125fccb838c218bbacf3d9038c433d3a5
Alex Pentland <apentlander@gmail.com>
2013-09-11 13:50:07 +1000

    Added conversion to opencv

---
+++ asst1/build/CMakeFiles/visual.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/alex/COMP3431-Assignment1/asst1/src/beacons.cpp" "/home/alex/COMP3431-Assignment1/asst1/build/CMakeFiles/visual.dir/src/beacons.cpp.o"
+  "/home/alex/COMP3431-Assignment1/asst1/src/nodes/visual.cpp" "/home/alex/COMP3431-Assignment1/asst1/build/CMakeFiles/visual.dir/src/nodes/visual.cpp.o"
+  "/home/alex/COMP3431-Assignment1/asst1/src/path.cpp" "/home/alex/COMP3431-Assignment1/asst1/build/CMakeFiles/visual.dir/src/path.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "/opt/ros/groovy/stacks/navigation/robot_pose_ekf/srv_gen/cpp/include"
+  "/opt/ros/groovy/include"
+  "/opt/ros/groovy/include/opencv"
+  "/opt/ros/groovy/include/bfl"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ asst1/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+export CATKIN_TEST_RESULTS_DIR="/home/alex/COMP3431-Assignment1/asst1/build/test_results"
+export ROS_TEST_RESULTS_DIR="/home/alex/COMP3431-Assignment1/asst1/build/test_results"
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/alex/COMP3431-Assignment1/asst1/build/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/alex/COMP3431-Assignment1/asst1/build/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/alex/COMP3431-Assignment1/asst1/build/devel/lib:$LD_LIBRARY_PATH"
+export PATH="/home/alex/COMP3431-Assignment1/asst1/build/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/alex/COMP3431-Assignment1/asst1/build/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PYTHONPATH="/home/alex/COMP3431-Assignment1/asst1/build/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/alex/COMP3431-Assignment1/asst1/build/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/alex/COMP3431-Assignment1/asst1:/opt/ros/groovy/share:/opt/ros/groovy/stacks"
