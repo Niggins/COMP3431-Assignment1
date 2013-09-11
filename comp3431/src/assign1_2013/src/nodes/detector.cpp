c4abb83da5d71519b8d07225f5b8bcd48c66c89f
Slawomir Grzebien <slawek@ubuntu.(none)>
2013-09-11 22:45:27 +1000

    detector is now with filter reducing noise

--- comp3431/src/assign1_2013/src/nodes/detector.cpp
+++ comp3431/src/assign1_2013/src/nodes/detector.cpp
@@ -15,7 +15,8 @@ class ImageConverter
   image_transport::ImageTransport it_;
   image_transport::Subscriber image_sub_;
   image_transport::Publisher image_pub_;
-  cv::Mat src, proc, out;
+  cv::Mat src, proc, out, erosion, dilation, elementRect, elementCross, coloured;
+
 
 public:
   ImageConverter()
@@ -23,13 +24,15 @@ public:
   {
     image_pub_ = it_.advertise("out", 1);
     image_sub_ = it_.subscribe("in", 1, &ImageConverter::imageCb, this);
-
     cv::namedWindow(WINDOW);
+    cv::namedWindow("Erosion");
+    cv::namedWindow("No noise");
+    cv::namedWindow("Coloured");
   }
 
   ~ImageConverter()
   {
-    cv::destroyWindow(WINDOW);
+    cv::destroyAllWindows();
   }
 
   void imageCb(const sensor_msgs::ImageConstPtr& msg)
@@ -38,7 +41,6 @@ public:
     try
     {
       cv_ptr = cv_bridge::toCvCopy(msg, enc::BGR8);
-      //IplImage *cvImage = cv_ptr;
       src = cv_ptr->image.clone();
 
     }
@@ -47,61 +49,47 @@ public:
       ROS_ERROR("cv_bridge exception: %s", e.what());
       return;
     }
+    image_pub_.publish(cv_ptr->toImageMsg()); //needs to be moved
+   image_processing();
+  }
 
+  void image_processing(){
     cv::cvtColor(src, proc, CV_BGR2HSV);
     cv::blur(proc, proc, cv::Size(3, 3));
+    cv::inRange(proc, cv::Scalar(158, 120, 120), cv::Scalar(167, 200, 200), out);
+ //   cv::blur(out, out, cv::Size(3, 3));
+    //erozja
 
-    cv::inRange(proc, cv::Scalar(160, 100, 100), cv::Scalar(170, 255, 255), out);
+       //setting dimensions of new images
+    erosion.create(proc.rows, proc.cols, proc.type());
+    dilation.create(proc.rows, proc.cols, proc.type());
 
-    //cv::Mat threshold_output;
-    //vector<vector<cv::Point> > contours;
+       int numberOfIterations = 5;
+       int erosion_size=1; //width of the "brush" for the process
+       elementCross = getStructuringElement( cv::MORPH_CROSS, cv::Size( 2*erosion_size + 1, 2*erosion_size+1 ),
+                                              cv::Point( erosion_size, erosion_size ) );
+       elementRect = getStructuringElement( cv::MORPH_RECT, cv::Size( 2*erosion_size + 1, 2*erosion_size+1 ),
+                                                     cv::Point( erosion_size, erosion_size ) );
 
+       //erozja wlasciwa
+       erode(out, erosion, elementCross);
+       medianBlur(erosion, erosion, 5);
+       dilate(erosion, dilation, elementRect);
 
+//       cv::imshow("Erosion", erosion);
+       cv::imshow("No noise", dilation);
 
-    /*IplImage cvImage = src;
-    IplImage* cvImagePtr = &cvImage;
-    /*IplImage cvImageHsv;
-    cvCvtColor(cvImage, cvImageHsv, CV_BGR2HSV);
-    IplImage* imgYellow = cvCreateImage(cvGetSize(&cvImage), IPL_DEPTH_8U, 1);
-    cvInRangeS(&cvImage, cvScalar(22, 100, 100), cvScalar(30, 255, 255), imgYellow);
-    /*IplImage* imgBlue = cvCreateImage(cvGetSize(&cvImage), IPL_DEPTH_8U, 1);
-    cvInRangeS(&cvImage, cvScalar(90, 100, 100), cvScalar(125, 255, 255), imgBlue);
-    IplImage* imgGreen = cvCreateImage(cvGetSize(&cvImage), IPL_DEPTH_8U, 1);
-    cvInRangeS(&cvImage, cvScalar(44, 100, 100), cvScalar(65, 255, 255), imgGreen);
-    IplImage* imgPink = cvCreateImage(cvGetSize(&cvImage), IPL_DEPTH_8U, 1);
-    cvInRangeS(&cvImage, cvScalar(160, 100, 100), cvScalar(170, 255, 255), imgPink);*/
-    //detect_beacons(cvImagePtr);
-
-    cv::imshow(WINDOW, out);
+ //   cv::imshow(WINDOW, out);
     cv::waitKey(3);
 
-    image_pub_.publish(cv_ptr->toImageMsg());
   }
-	/*void detect_beacons(IplImage* cvImage){
-		//color  detection
-		IplImage* frame = 0;
-		IplImage* imgThresh = 0;
-		frame = cvCloneImage(cvImage);
-		cvSmooth(frame, frame, CV_GAUSSIAN,3,3);
-
-		IplImage* imgHSV = cvCreateImage(cvGetSize(frame), IPL_DEPTH_8U, 3);
-		cvCvtColor(frame, imgHSV, CV_BGR2HSV);
-
-		imgThresh=cvCreateImage(cvGetSize(imgHSV),IPL_DEPTH_8U, 1);
-		cvInRangeS(imgHSV, cvScalar(160,100,100), cvScalar(170,255,255), imgThresh);
-		//cvSmooth(imgThresh, imgThresh, CV_GAUSSIAN,3,3);
-		cvShowImage("Found beacons", imgThresh);
-
-	}*/
 
 };
 
 int main(int argc, char** argv)
 {
-  cvNamedWindow("Found beacons");
   ros::init(argc, argv, "image_converter");
   ImageConverter ic;
   ros::spin();
-  cvDestroyWindow("Found beacons");
   return 0;
 }
