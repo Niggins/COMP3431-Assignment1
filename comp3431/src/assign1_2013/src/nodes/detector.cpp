358dc149eb60b004ab5f553c6e539326620f2481
Alex Pentland <apentlander@gmail.com>
2013-09-13 12:35:52 +1000

    Now detects the center of beacons and is commented

--- comp3431/src/assign1_2013/src/nodes/detector.cpp
+++ comp3431/src/assign1_2013/src/nodes/detector.cpp
@@ -17,9 +17,9 @@ class ImageConverter
   image_transport::ImageTransport it_;
   image_transport::Subscriber image_sub_;
   image_transport::Publisher image_pub_;
-  cv::Mat src, proc, out, erosion, dilation, elementRect, elementCross, coloured;
+  cv::Mat src, proc, out, erosion, dilation, elementRect, elementCross;
 
-  cv::Mat threshold_output, drawing;
+  cv::Mat threshold_output;
   std::vector<std::vector<cv::Point> > contours;
   std::vector<cv::Vec4i> hierarchy;
 
@@ -56,103 +56,115 @@ public:
     cv::cvtColor(src, proc, CV_BGR2HSV);
     cv::GaussianBlur(proc, proc, cv::Size(3, 3), 0, 0);
 
-    cv::Mat colorOut[4], combOut[2], coloredLayers[4];
+    cv::Mat filteredLayer[4], combOut[2], coloredLayer[4];
     cv::Scalar boxColor[4];
-    // Pink Layer
-    cv::inRange(proc, cv::Scalar(158, 120, 120), cv::Scalar(168, 200, 200), colorOut[0]);
-    //boxColor[0] = cv::Scalar(160, 230, 255);
+    // Pink layer filter
+    cv::inRange(proc, cv::Scalar(158, 120, 120), cv::Scalar(168, 200, 200), filteredLayer[0]);
     boxColor[0] = cv::Scalar(127, 0, 255);
-    // Blue Layer
-    cv::inRange(proc, cv::Scalar(100, 127, 77), cv::Scalar(110, 192, 179), colorOut[1]);
+    // Blue layer filter
+    cv::inRange(proc, cv::Scalar(100, 127, 77), cv::Scalar(110, 192, 179), filteredLayer[1]);
     boxColor[1] = cv::Scalar(255, 0, 0);
-    // Green layer
-    cv::inRange(proc, cv::Scalar(75, 127, 35), cv::Scalar(90, 230, 110), colorOut[2]);
+    // Green layer filter
+    cv::inRange(proc, cv::Scalar(75, 127, 35), cv::Scalar(90, 230, 110), filteredLayer[2]);
     boxColor[2] = cv::Scalar(0, 255, 0);
-    // Yellow layer
-    cv::inRange(proc, cv::Scalar(22, 178, 102), cv::Scalar(30, 255, 179), colorOut[3]);
+    // Yellow layer filter
+    cv::inRange(proc, cv::Scalar(22, 178, 102), cv::Scalar(30, 255, 179), filteredLayer[3]);
     boxColor[3] = cv::Scalar(0, 255, 255);
-    cv::String name[4] = {"Pink", "Blue", "Green", "Yellow"};
 
-    //setting dimensions of new images
+    // Filtering stuff
     erosion.create(proc.rows, proc.cols, proc.type());
     dilation.create(proc.rows, proc.cols, proc.type());
 
     std::vector<cv::Rect> pinkRect;
+    // Iterate through the different color layers
     for (int k=0; k < 4; k++)
     {
+      // Filter out noise
       int numberOfIterations = 5;
       int erosion_size=1; //width of the "brush" for the process
       elementCross = cv::getStructuringElement(cv::MORPH_CROSS, cv::Size( 2*erosion_size + 1, 2*erosion_size + 1),
                                                cv::Point(erosion_size, erosion_size));
       elementRect = cv::getStructuringElement(cv::MORPH_RECT, cv::Size( 2*erosion_size + 1, 2*erosion_size + 1 ),
                                               cv::Point(erosion_size, erosion_size));
-
-      //erozja wlasciwa
-      cv::erode(colorOut[k], erosion, elementCross);
+      cv::erode(filteredLayer[k], erosion, elementCross);
       cv::medianBlur(erosion, erosion, 5);
-      cv::dilate(erosion, colorOut[k], elementRect);
+      cv::dilate(erosion, filteredLayer[k], elementRect);
 
-      cv::findContours(colorOut[k], contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0));
+      // Find the contours of the current color layer
+      cv::findContours(filteredLayer[k], contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0));
       std::vector<cv::Rect> minRect(contours.size());
-      coloredLayers[k] = cv::Mat::zeros(colorOut[k].size(), CV_8UC3);
+      coloredLayer[k] = cv::Mat::zeros(filteredLayer[k].size(), CV_8UC3);
       for(int i = 0; i < contours.size(); i++)
       { 
+        // Create rectangles containing the counours of the current color
         minRect[i] = cv::boundingRect(cv::Mat(contours[i]));;
         minRect[i].height += 7;
 
+        // Store all the pink rectangles
         if (k == 0)
         {
           pinkRect.push_back(minRect[i]);
         }
+
+        // If the current color is not pink, iterate through the pink rectangles
         else
         {
           for (int a = 0; a < pinkRect.size(); a++)
           {
+            // Checks if a pink rectangle intersects the current colored rectangle
             if ((pinkRect[a] & minRect[i]).area())
             {
-              cv::Point circ;
-              if (k == 1)
+              cv::Point center;
+              // Checks if pink rectangle is on top
+              if (pinkRect[a].y < minRect[i].y)
               {
-                if (pinkRect[a].y < minRect[i].y)
-                  circ = calcRectMid(pinkRect[a], minRect[i]);
-                else
-                  circ = calcRectMid(minRect[i], pinkRect[a]);
+                center = calcRectMid(pinkRect[a], minRect[i]);
+                if (k == 1)
+                {
+
+                }
+                if (k == 2)
+                {
+
+                }
+                if (k == 3)
+                {
+
+                }
               }
-              else if (k == 2)
-              {
-                if (pinkRect[a].y < minRect[i].y)
-                  circ = calcRectMid(pinkRect[a], minRect[i]);
-                else
-                  circ = calcRectMid(minRect[i], pinkRect[a]);
-              }
+              // Otherwise pink rectangle is on the bottom
               else
               {
-                if (pinkRect[a].y < minRect[i].y)
-                  circ = calcRectMid(pinkRect[a], minRect[i]);
-                else
-                  circ = calcRectMid(minRect[i], pinkRect[a]);
+                center = calcRectMid(minRect[i], pinkRect[a]);
+                if (k == 1)
+                {
+
+                }
+                if (k == 2)
+                {
+
+                }
+                if (k == 3)
+                {
+                  
+                }
               }
-              cv::circle(coloredLayers[k], circ, 3, cv::Scalar(0, 0, 255), -1);
+              // Draw a red dot at the center point of the 2 rectangles
+              cv::circle(coloredLayer[k], center, 3, cv::Scalar(0, 0, 255), -1);
             }
           }
         }
       }
 
+      // Draw bounding rectangles the same color as the color contour
       for(int i = 0; i < contours.size(); i++ )
       {
-        // contour
-        //cv::drawContours(drawing, contours, i, boxColor[k], 1, 8, std::vector<cv::Vec4i>(), 0, cv::Point());
-        cv::rectangle(coloredLayers[k], minRect[i], boxColor[k]);
+        cv::rectangle(coloredLayer[k], minRect[i], boxColor[k]);
       }
-      //cv::imshow(name[k], coloredLayers[k]);
     }
 
-    /*cv::bitwise_or(colorOut[0], colorOut[1], combOut[0]);
-    cv::bitwise_or(colorOut[2], colorOut[3], combOut[1]);
-    cv::bitwise_or(combOut[0], combOut[1], threshold_output);*/
-
-    cv::bitwise_or(coloredLayers[0], coloredLayers[1], combOut[0]);
-    cv::bitwise_or(coloredLayers[2], coloredLayers[3], combOut[1]);
+    cv::bitwise_or(coloredLayer[0], coloredLayer[1], combOut[0]);
+    cv::bitwise_or(coloredLayer[2], coloredLayer[3], combOut[1]);
     cv::bitwise_or(combOut[0], combOut[1], threshold_output);
 
     cv::imshow(WINDOW, threshold_output);
